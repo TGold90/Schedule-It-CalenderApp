5048900a5565b7b0bda136f008a63a1c44fb58aa
TGOLD <thomaswgold90@gmail.com>
2022-07-14 10:52:04 -0600

    added first test iteration of a timeblock

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var todayDate = $("#currentDay");
 
 
-function showDate() {
+function showDate() { // show date and time
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     todayDate.text(rightNow);
   }
 
-  setInterval(showDate, 1000);
+setInterval(showDate, 1000);
