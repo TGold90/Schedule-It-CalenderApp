4eaae0ed251b73c91184accb1c8f56873499f38f
TGOLD <thomaswgold90@gmail.com>
2022-07-19 18:30:05 -0600

    readme and JS cleanup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Schedule-It-CalenderApp
+
+## Description
+
+Schedule It! Work Day Edition is a simple scheduling application where a user can create events between the timeblocks of 9am and 5pm. 
+
+The day and time is accurately displayed in the header, and there is a text area and save button for each time slot available. The time slots are color coded to let the user know if a time slot is in the past, the presetn or the future.
+
+## Usage
+
+To add an event, the user can select a time slot and enter text. To save an event, the user will click the "Add Event" button. 
+
+Added events will persist on the apge even through a page reload.
+
+## Link and Screenshot
+
--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-//Pseudo Code
-//Add date and time to to jumbotron header (moment)
-//add time blocks for each hour
-//color code each time block to show past, present, future
-//add input and save button to each time block
-//save input to local storage
+
 const container = $(".container");
 const timeSlots = ["9AM", "10AM", "11Am", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 const timeId = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const saveBtn = $(".saveBtn");
 const timeBlock = $('.time-block');
 const description = $(".description");
@@ -22,21 +16,6 @@ function showDate() { // Display current day and time
   }
 
 setInterval(showDate, 1000);
-
-// for(let i = 0; i < timeSlots.length; i++) { //this is the javascript option to get 
-//   let row = $("<div class='row time-block'>").attr("id", timeId[i]);//add ID to each iteration
-
-//   let hour = $("<div class='col-1 hour'>");
-//   let textArea = $("<textarea class='col-md-10 description'>");
-//   textArea.attr("id", timeSlots[i]);
-//   let saveBtn = $("<button class='col-1 btn saveBtn'>").text('Add Event');
-//   container.append(row);
-
-//   hour.text(timeSlots[i]);
-//   row.append(hour);
-//   row.append(textArea);
-//   row.append(saveBtn);
-// }
 
 function timeState(){
   let currentHour = moment().hours(); 
